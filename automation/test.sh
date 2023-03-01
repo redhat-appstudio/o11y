e9531f093f3d9e1e63ce5cd33fa5fb3987fa69d1
Yftach Herzog <yherzog@redhat.com>
2023-03-01 11:39:36 +0200

    test(STONEO11Y-43): add promql unit testing
    Install promtool to allow promql unit testing and provide example
    test cases.
    
    Signed-off-by: Yftach Herzog <yherzog@redhat.com>

--- automation/test.sh
+++ automation/test.sh
@@ -2,7 +2,17 @@
 
 main() {
     ls -l
+    install_promtool
+
     echo "Running tests"
+    ./promtool test rules test/promql/*
+}
+
+install_promtool() {
+    echo "Installing promtool..."
+    VERSION="2.42.0"
+    curl -OJL "https://github.com/prometheus/prometheus/releases/download/v${VERSION}/prometheus-$VERSION.linux-amd64.tar.gz"
+    tar xvzf "prometheus-$VERSION.linux-amd64.tar.gz" "prometheus-$VERSION.linux-amd64"/promtool --strip-components=1
 }
 
 if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
