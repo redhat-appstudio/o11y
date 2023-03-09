2ab92c341b11718f734c834b25405c145f11dce0
Yftach Herzog <yherzog@redhat.com>
2023-03-09 14:26:18 +0200

    test(STONEO11Y-56): extract prometheus rules for tests
    Our Prometheus rules are stored as Kubernetes objects, but promtool
    which is used for unit tests requires them to be on their own.
    This change extract the rules to a rule file as part the script that
    runs the unit tests.
    
    Signed-off-by: Yftach Herzog <yherzog@redhat.com>

--- automation/test.sh
+++ automation/test.sh
@@ -1,18 +1,39 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 main() {
-    ls -l
     install_promtool
+    install_yq
+
+    echo "Extract Prometheus rules"
+    ./yq ".spec" prometheus/base/prometheus.rules.yaml > test/promql/extracted-rules.yaml
 
     echo "Running tests"
-    ./promtool test rules test/promql/*
+    ./promtool test rules test/promql/tests/*
 }
 
 install_promtool() {
+    if test -f "./promtool"; then
+        echo "Skipping promtool installation"
+        return
+    fi
     echo "Installing promtool..."
     VERSION="2.42.0"
     curl -OJL "https://github.com/prometheus/prometheus/releases/download/v${VERSION}/prometheus-$VERSION.linux-amd64.tar.gz"
     tar xvzf "prometheus-$VERSION.linux-amd64.tar.gz" "prometheus-$VERSION.linux-amd64"/promtool --strip-components=1
+    rm -f prometheus-$VERSION.linux-amd64.tar.gz
+}
+
+install_yq() {
+    if test -f "./yq"; then
+        echo "Skipping yq installation"
+        return
+    fi
+    echo "Installing yq..."
+    VERSION="4.31.2"
+    curl -OJL "https://github.com/mikefarah/yq/releases/download/v$VERSION/yq_linux_amd64.tar.gz"
+    tar xvzf yq_linux_amd64.tar.gz ./yq_linux_amd64
+    rm -f yq_linux_amd64.tar.gz
+    mv yq_linux_amd64 yq
 }
 
 if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
