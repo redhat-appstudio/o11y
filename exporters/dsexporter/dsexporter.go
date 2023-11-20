c7da36042833c3d615fd690ecb89d2821a723c0f
Kousalya Lakshmanan <klakshma@redhat.com>
2023-11-20 13:41:02 -0600

    chore(RHTAPWATCH-565): Modify the service into Prometheus exporter
    - Add exporter created with summy metrics
    - Add the unit test file
    
    Signed-off-by: Kousalya Lakshmanan <klakshma@redhat.com>

--- exporters/dsexporter/dsexporter.go
+++ exporters/dsexporter/dsexporter.go
@@ -1,20 +1,52 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-func ping(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "pong")
+type CustomCollector struct {
+	requestCounter prometheus.Counter
 }
 
-func main() {
-	http.HandleFunc("/ping", ping)
-
-	fmt.Println("Starting dummy service on :8090")
-	err := http.ListenAndServe(":8090", nil)
-	if err != nil {
-		fmt.Println("Error starting dummy service:", err)
+// Creating a new instance of CustomCollector.
+func NewCustomCollector() *CustomCollector {
+	return &CustomCollector{
+		requestCounter: prometheus.NewCounter(prometheus.CounterOpts{
+			Name: "request_count",
+			Help: "Number of requests handled by the handler",
+		}),
 	}
+}
+
+// Describe method sends descriptions of the metrics to Prometheus.
+// When Prometheus scrapes the /metrics endpoint of the exporter,
+// it first calls the Describe method to get a description of all the metrics.
+func (e *CustomCollector) Describe(ch chan<- *prometheus.Desc) {
+	e.requestCounter.Describe(ch)
+}
+
+// Collect method sends the current values of the metrics to Prometheus.
+// After Prometheus understands what metrics are available (using the `Describe` method),
+// it then calls the `Collect` method to actually get the values of those metrics.
+func (e *CustomCollector) Collect(ch chan<- prometheus.Metric) {
+	e.requestCounter.Collect(ch)
+}
+
+// Using a separate pedantic registry ensures that only your custom metric is exposed on the "/metrics" endpoint,
+// providing a cleaner and more focused view. Also reduces potential noise from unrelated metrics.
+func main() {
+	reg := prometheus.NewPedanticRegistry()
+	exporter := NewCustomCollector()
+	reg.MustRegister(exporter)
+	http.Handle("/metrics", promhttp.HandlerFor(
+		reg,
+		promhttp.HandlerOpts{
+			EnableOpenMetrics: true,
+			Registry:          reg,
+		},
+	))
+	http.ListenAndServe(":8090", nil)
 }
